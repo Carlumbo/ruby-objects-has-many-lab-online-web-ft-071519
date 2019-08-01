6592a1b32fe708c0dea5dfded30e7c00e98118db
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-01 19:06:30 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -4,4 +4,15 @@ class Post
   @@all
   
   def initialize*(name)
-    @name
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @all
+  end 
+  
+  def author_name 
+    author.name if author 
+  end 
+end 
