36da0a6a687552d661b9819abc0b44e8b0fc0958
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-01 18:58:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,6 +13,10 @@ class Artist
   end 
   
   def add_song(song)
-    @song = Song.new 
+    song.artist = self 
+  end 
+
+  def add_song_by_name(song)
+    song = Song.new 
     song.artist = self 
   end 
